34fbe0bba4e2534a6b256eef7ed17bf7d34da5ff
Aryan756-web <aryankaradkar2312@gmail.com>
2026-04-07 12:26:11 +0530

    added delete visitor feature

--- backend/controllers/visitorController.js
+++ backend/controllers/visitorController.js
@@ -33,4 +33,19 @@ const getVisitors = async (req, res) => {
 }
 };
 
-module.exports = { addVisitor, getVisitors };
+//Delete Visitor
+const deleteVisitor = async (req, res) => {
+  try {
+    const visitor = await Visitor.findByIdAndDelete(req.params.id);
+
+    if (!visitor) {
+      return res.status(404).json({ message: "Visitor not found" });
+    }
+
+    res.json({ message: "Visitor deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting visitor" });
+  }
+};
+
+module.exports = { addVisitor, getVisitors, deleteVisitor };
--- backend/routes/visitorRoutes.js
+++ backend/routes/visitorRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addVisitor, getVisitors } = require("../controllers/visitorController");
+const { addVisitor, getVisitors, deleteVisitor } = require("../controllers/visitorController");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, addVisitor);
 router.get("/", protect, getVisitors);
+router.delete("/:id", protect, deleteVisitor);
 
 module.exports = router;
